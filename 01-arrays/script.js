a7ee21a35921fd0d580a717e3599cf6126a1a742
DarioConiglione <dariocng13@gmail.com>
2025-10-08 15:16:14 +0100

    ex. 8 done

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -51,4 +51,6 @@ console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+let verifyEmptyness = teachers.length === 0;
+const isTeachersEmpty = verifyEmptyness;
+console.log(isTeachersEmpty);
