4452ad15a2cf82a68c20c8b9fb2531738c6a7291
DarioConiglione <dariocng13@gmail.com>
2025-10-09 09:15:32 +0100

    ex 4 done

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -38,7 +38,18 @@ console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+let isFabioPresent = false;
+
+for (let i = 0; i < teachers.length; i++) {
+  const thisTeacher = teachers[i]
+  if (teachers[i] === "Fabio") {
+    isFabioPresent = true;
+    break;
+   } 
+  }
+  console.log(isFabioPresent)
+
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
